2bee3f1977f35c8b7fe63c64127dcf3f63be6f17
canewsin <canews.in@gmail.com>
2022-12-16 06:12:19 +0530

    Use extension methods for $Icon class

--- lib/src/widgets/icon.dart
+++ lib/src/widgets/icon.dart
@@ -6,29 +6,41 @@ import 'package:flutter_eval/src/sky_engine/ui/painting.dart';
 import 'package:flutter_eval/src/sky_engine/ui/text.dart';
 import 'package:flutter_eval/src/widgets/framework.dart';
 import 'package:flutter_eval/src/widgets/icon_data.dart';
+import 'package:flutter_eval/src/extensions/extensions.dart';
 
 class $Icon implements $Instance {
-  static const $type = BridgeTypeRef.spec(BridgeTypeSpec('package:flutter/src/widgets/icon.dart', 'Icon'));
+  static const $type = BridgeTypeRef.spec(
+    BridgeTypeSpec('package:flutter/src/widgets/icon.dart', 'Icon'),
+  );
 
-  static const $declaration =
-      BridgeClassDef(BridgeClassType($type, isAbstract: false, $extends: $StatelessWidget$bridge.$type),
-          constructors: {
-            '': BridgeConstructorDef(BridgeFunctionDef(returns: BridgeTypeAnnotation($type), params: [
-              BridgeParameter('icon', BridgeTypeAnnotation($IconData.$type), false),
-            ], namedParams: [
-              BridgeParameter('key', BridgeTypeAnnotation($Key.$type, nullable: true), true),
-              BridgeParameter('size', BridgeTypeAnnotation(BridgeTypeRef.type(RuntimeTypes.doubleType)), true),
-              BridgeParameter('color', BridgeTypeAnnotation($Color.$type, nullable: true), true),
-              BridgeParameter('semanticLabel',
-                  BridgeTypeAnnotation(BridgeTypeRef.type(RuntimeTypes.stringType), nullable: true), true),
-              BridgeParameter('textDirection', BridgeTypeAnnotation($TextDirection.$type, nullable: true), true),
-            ]))
-          },
-          methods: {},
-          getters: {},
-          setters: {},
-          fields: {},
-          wrap: true);
+  static final $declaration = BridgeClassDef(
+    const BridgeClassType(
+      $type,
+      isAbstract: false,
+      $extends: $StatelessWidget$bridge.$type,
+    ),
+    constructors: {
+      '': BridgeConstructorDef(
+        BridgeFunctionDef(
+          returns: $type.annotate,
+          params: ['icon'.wrapped($IconData.$type)],
+          namedParams: [
+            'key'.wrappedOptionalNullable($Key.$type),
+            'size'.wrappedOptional(RuntimeTypes.doubleType.bridged),
+            'color'.wrappedOptionalNullable($Color.$type),
+            'semanticLabel'
+                .wrappedOptionalNullable(RuntimeTypes.stringType.bridged),
+            'textDirection'.wrappedOptionalNullable($TextDirection.$type),
+          ],
+        ),
+      )
+    },
+    methods: {},
+    getters: {},
+    setters: {},
+    fields: {},
+    wrap: true,
+  );
 
   const $Icon.wrap(this.$value);
 
